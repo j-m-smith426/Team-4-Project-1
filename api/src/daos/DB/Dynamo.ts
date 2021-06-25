a9d51dc458eb7fff29f71db5fb3d606f8d30ed53
j-m-smith426 <joabsmith4226@gmail.com>
2021-06-25 13:08:41 -0400

    Initial Project Setup

---
+++ api/src/daos/DB/Dynamo.ts
@@ -0,0 +1,23 @@
+import {DynamoDBClient} from "@aws-sdk/client-dynamodb"
+import {DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb"
+const REGION:string = "us-east-2"
+const ddb:DynamoDBClient = new DynamoDBClient({region: REGION})
+
+const marshallOptions = {
+    // Whether to automatically convert empty strings, blobs, and sets to `null`.
+    convertEmptyValues: false, // false, by default.
+    // Whether to remove undefined values while marshalling.
+    removeUndefinedValues: false, // false, by default.
+    // Whether to convert typeof object to map attribute.
+    convertClassInstanceToMap: false, // false, by default.
+};
+
+const unmarshallOptions = {
+    // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
+    wrapNumbers: false, // false, by default.
+};
+
+const translateConfig = { marshallOptions, unmarshallOptions };
+const ddbDoc = DynamoDBDocumentClient.from(ddb, translateConfig);
+
+export {ddb, ddbDoc};
