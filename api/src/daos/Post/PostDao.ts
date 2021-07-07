a13fe08a35d7a3a59e91c7bbd7ae9b9eb657e11b
j-m-smith426 <joabsmith4226@gmail.com>
2021-07-07 14:30:09 -0400

    pretty

--- api/src/daos/Post/PostDao.ts
+++ api/src/daos/Post/PostDao.ts
@@ -1,51 +1,49 @@
-import { IComment } from '@entities/Post';
-
-
-
+import { IComment } from "@entities/Post";
 
 export interface IPostDao {
-    getOne: (postID: string) => Promise<IComment|undefined>;
-    getAllPageComments: (subjectID:string) => Promise<any>;
-    getAllPostComments: (postID:string) => Promise<IComment[]|undefined>;
-    addComment: (comment: IComment) => Promise<void>;
-    //update: (user: IUser) => Promise<void>;
-    //delete: (id: number) => Promise<void>;
+  getOne: (postID: string) => Promise<IComment | undefined>;
+  getAllPageComments: (subjectID: string) => Promise<any>;
+  getAllPostComments: (postID: string) => Promise<IComment[] | undefined>;
+  addComment: (comment: IComment) => Promise<void>;
+  //update: (user: IUser) => Promise<void>;
+  //delete: (id: number) => Promise<void>;
 }
 
 class PostDao implements IPostDao {
-    /**
-     * 
-     * @param postID 
-     * @returns 
-     */  
-    public async getOne(postID:string): Promise<IComment|undefined>{
-        //TODO
-        return Promise.resolve(undefined);
-    }
-    
-    public async getAllPostComments(postID:string) :Promise<IComment[]|undefined>{
-        //TODO
-        return Promise.resolve(undefined);
-    }
-    
-    
-    /**
-     *
-     * @param comment
-     */
-    public async addComment(comment: IComment): Promise<void> {
-        // TODO
-       return Promise.resolve(undefined);
-   }
+  /**
+   *
+   * @param postID
+   * @returns
+   */
 
+  public async getOne(postID: string): Promise<IComment | undefined> {
+    //TODO
+    return Promise.resolve(undefined);
+  }
 
-    /**
-     *
-     */
-    public getAllPageComments(): Promise<any> {
-         // TODO
-        return Promise.resolve([]);
-    }
+  public async getAllPostComments(
+    postID: string
+  ): Promise<IComment[] | undefined> {
+    //TODO
+    return Promise.resolve(undefined);
+  }
+
+  /**
+   *
+   * @param comment
+   */
+  public async addComment(comment: IComment): Promise<void> {
+    // TODO
+    return Promise.resolve(undefined);
+  }
+
+  /**
+   *
+   */
+  public getAllPageComments(): Promise<any> {
+    // TODO
+    return Promise.resolve([]);
+  }
 }
 
 export default PostDao;
--- api/src/daos/User/UserDao.mock.ts
+++ api/src/daos/User/UserDao.mock.ts
@@ -1,66 +1,64 @@
-import User, { IUser } from '@entities/User';
-import { getRandomInt } from '@shared/functions';
-import { IUserDao } from './UserDao';
-import MockDaoMock from '../MockDb/MockDao.mock';
-import { DeleteCommand, GetCommand, PutCommand, QueryCommand, ScanCommand } from '@aws-sdk/lib-dynamodb';
-import { ddbDoc } from '../DB/Dynamo';
-
-
+import User, { IUser } from "@entities/User";
+import { getRandomInt } from "@shared/functions";
+import { IUserDao } from "./UserDao";
+import MockDaoMock from "../MockDb/MockDao.mock";
+import {
+  DeleteCommand,
+  GetCommand,
+  PutCommand,
+  QueryCommand,
+  ScanCommand,
+} from "@aws-sdk/lib-dynamodb";
+import { ddbDoc } from "../DB/Dynamo";
 
 const TABLE = "Scouter";
 class UserDao implements IUserDao {
-    public table = TABLE;
+  public table = TABLE;
 
-    public async getOne(username: string): Promise<IUser | null> {
+  public async getOne(username: string): Promise<IUser | null> {
+    const params = {
+      TableName: TABLE,
+      Key: {
+        TYPEID: "U#" + username,
+        REFERENCE: "0",
+      },
+    };
+    try {
+      const data = await ddbDoc.send(new GetCommand(params));
 
-        const params = {
-            TableName: TABLE,
-            Key:{
-                TYPEID:'U#'+username,
-                REFERENCE:"0",
-            },
-        };
-        try {
-            const data = await ddbDoc.send(new GetCommand(params));
-        
-           
-            return data.Item as IUser;
-          } catch (err) {
-            console.log("Error", err);
-          }
-
-        return null;
+      return data.Item as IUser;
+    } catch (err) {
+      console.log("Error", err);
     }
 
+    return null;
+  }
 
-    public async getAll() {
-        const params = {
-            TableName: TABLE,
-            KeyConditionExpression: "begins_with(TYPEID, :utag)",
-            ExpressionAttributeValues: {
-                ":utag": "U#"
-              }
-        };
-        try {
-            const data = await ddbDoc.send(new ScanCommand(params));
-            return Promise.resolve(data.Items);
-        } catch (err) {
-            console.log("Error", err);
-        }
+  public async getAll() {
+    const params = {
+      TableName: TABLE,
+      KeyConditionExpression: "begins_with(TYPEID, :utag)",
+      ExpressionAttributeValues: {
+        ":utag": "U#",
+      },
+    };
+    try {
+      const data = await ddbDoc.send(new ScanCommand(params));
+      return Promise.resolve(data.Items);
+    } catch (err) {
+      console.log("Error", err);
     }
+  }
 
+  public async add(user: IUser): Promise<void> {
+    const params = {
+      TableName: this.table,
+      Item: user,
+    };
+    await ddbDoc.send(new PutCommand(params));
+  }
 
-    public async add(user: IUser): Promise<void> {
-       
-        const params = {
-            TableName: this.table,
-            Item: user
-        }
-        await ddbDoc.send(new PutCommand(params));
-    }
-
-
-    /*public async update(user: IUser): Promise<void> {
+  /*public async update(user: IUser): Promise<void> {
         const db = await super.openDb();
         for (let i = 0; i < db.users.length; i++) {
             if (db.users[i].id === user.id) {
