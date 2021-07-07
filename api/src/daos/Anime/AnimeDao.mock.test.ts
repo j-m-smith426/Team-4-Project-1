a662445ae22830d3dafbf617e5de46092852dc25
Matthew Hanrahan <sunracer1977@gmail.com>
2021-07-07 00:41:49 -0400

    added anime test and get functions

---
+++ api/src/daos/Anime/AnimeDao.mock.test.ts
@@ -0,0 +1,21 @@
+import { IAnime } from "@entities/Anime";
+import AnimeDao from "./AnimeDao.mock";
+
+
+async function addGet(){
+    test('Testing the adding and retreiving of One User',async() =>{
+        let id = 'testId';
+        let genres = ['fighting'];
+        let name = 'DBZ';
+        let testUser:IAnime = {
+            TYPEID:'A#'+id,
+            REFERENCE:"0",
+            genres,
+            name:name
+        };
+            const anime = new AnimeDao();
+        await anime.add(testUser);
+        expect(await anime.getOne(id)).toStrictEqual(testUser);
+    })
+}
+addGet();
